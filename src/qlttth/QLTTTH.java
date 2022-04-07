3323bc8f845b03f418ea3b8a567adbe2fcadb314
IamGroot2001 <daohoanh01637788488@gmail.com>
2022-04-07 09:39:04 +0700

    Tạo project

---
+++ src/qlttth/QLTTTH.java
@@ -0,0 +1,11 @@
+package qlttth;
+
+public class QLTTTH {
+    public static void main(String[] args) {
+        DangNhapForm dangNhapForm = new DangNhapForm();
+        dangNhapForm.setLocationRelativeTo(null);
+        dangNhapForm.setVisible(true);
+        System.out.println("qlttth.QLTTTH.main()");
+    }
+    
+}
