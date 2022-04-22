d033a354974ca091ea3cc40e72d2da21edb8178e
IamGroot2001 <daohoanh01637788488@gmail.com>
2022-04-22 14:45:04 +0700

    ket noi database

--- src/qlttth/QLTTTH.java
+++ src/qlttth/QLTTTH.java
@@ -1,7 +1,33 @@
 package qlttth;
 
+import com.microsoft.sqlserver.jdbc.SQLServerXADataSource;
+import java.sql.*;
+
 public class QLTTTH {
     public static void main(String[] args) {
+        
+        var server = "DESKTOP-0EV3HR0\\SQLEXPRESS";
+        var user = "sa";
+        var password = "123456";
+        var db = "test";
+        var port = 1433;
+        
+        SQLServerXADataSource ds = new SQLServerXADataSource();
+        ds.setUser(user);
+        ds.setPassword(password);
+        ds.setDatabaseName(db);
+        ds.setPortNumber(port);
+        
+        try (Connection conn = ds.getConnection())
+        {
+            System.out.println("Ket noi thanh cong!!");
+            System.out.println(conn.getCatalog());
+        }
+        catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+        
         DangNhapForm dangNhapForm = new DangNhapForm();
         dangNhapForm.setLocationRelativeTo(null);
         dangNhapForm.setVisible(true);
